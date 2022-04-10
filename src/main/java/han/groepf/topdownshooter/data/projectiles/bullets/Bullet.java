74d004407deeedec2a8134450bd376d2b13f4f0b
Tim Barbiers <42208814+Barbierst@users.noreply.github.com>
2022-04-10 16:54:41 +0200

    Update src/main/java/han/groepf/topdownshooter/data/projectiles/bullets/Bullet.java

--- src/main/java/han/groepf/topdownshooter/data/projectiles/bullets/Bullet.java
+++ src/main/java/han/groepf/topdownshooter/data/projectiles/bullets/Bullet.java
@@ -3,5 +3,4 @@ package han.groepf.topdownshooter.data.projectiles.bullets;
 import han.groepf.topdownshooter.data.projectiles.Projectile;
 
 public class Bullet extends Projectile {
-    public int damage;
 }
