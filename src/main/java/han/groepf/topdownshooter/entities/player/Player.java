b6c5e1ebcf250de56fad9b2fc3d80031f05782c7
Tim Barbiers <tim@sandwave.io>
2022-04-15 09:53:54 +0200

    remove yaeger docs, implement basic player movement

--- src/main/java/han/groepf/topdownshooter/entities/player/Player.java
+++ src/main/java/han/groepf/topdownshooter/entities/player/Player.java
@@ -1,20 +1,68 @@
 package han.groepf.topdownshooter.entities.player;
 
 import com.github.hanyaeger.api.Coordinate2D;
+import com.github.hanyaeger.api.entities.Newtonian;
+import com.github.hanyaeger.api.entities.SceneBorderTouchingWatcher;
 import com.github.hanyaeger.api.entities.impl.DynamicSpriteEntity;
+import com.github.hanyaeger.api.scenes.SceneBorder;
 import com.github.hanyaeger.api.userinput.KeyListener;
 import javafx.scene.input.KeyCode;
 
 import java.util.Set;
 
-public class Player extends DynamicSpriteEntity implements KeyListener {
+public class Player extends DynamicSpriteEntity implements KeyListener, SceneBorderTouchingWatcher, Newtonian {
 
-    public Player(String resource, Coordinate2D initialLocation) {
-        super(resource, initialLocation);
+    /**
+     * This constructor abstracts away the resource selection and passes on the initial location to the super constructor
+     * Adds friction to stop the player after a key is released
+     * @param initialLocation
+     */
+    public Player(Coordinate2D initialLocation) {
+        super("sprites/player.png", initialLocation);
+
+        setGravityConstant(0);
+        setFrictionConstant(0.04);
     }
 
+    /**
+     * Lissen to the pressed key changed event and move the player in the correct direction
+     * @param pressedKeys
+     */
     @Override
-    public void onPressedKeysChange(Set<KeyCode> set) {
+    public void onPressedKeysChange(Set<KeyCode> pressedKeys) {
+        if(pressedKeys.contains(KeyCode.LEFT)){
+            setMotion(3,270d);
+        } else if(pressedKeys.contains(KeyCode.RIGHT)){
+            setMotion(3,90d);
+        } else if(pressedKeys.contains(KeyCode.UP)){
+            setMotion(3,180d);
+        } else if(pressedKeys.contains(KeyCode.DOWN)){
+            setMotion(3,0d);
+        }
+    }
 
+    /**
+     * When the player touches a border the speed is set to 0 to prevent leaving the game.
+     * @param sceneBorder
+     */
+    @Override
+    public void notifyBoundaryTouching(SceneBorder sceneBorder) {
+        setSpeed(0);
+        switch (sceneBorder) {
+            case TOP:
+                setAnchorLocationY(1);
+                break;
+            case BOTTOM:
+                setAnchorLocationY(getSceneHeight() - getHeight() - 1);
+                break;
+            case LEFT:
+                setAnchorLocationX(1);
+                break;
+            case RIGHT:
+                setAnchorLocationX(getSceneWidth() - getWidth() - 1);
+                break;
+            default:
+                break;
+        }
     }
 }
--- src/main/java/han/groepf/topdownshooter/scenes/GameScene.java
+++ src/main/java/han/groepf/topdownshooter/scenes/GameScene.java
@@ -1,13 +1,19 @@
 package han.groepf.topdownshooter.scenes;
 
+import com.github.hanyaeger.api.Coordinate2D;
 import com.github.hanyaeger.api.EntitySpawnerContainer;
 import com.github.hanyaeger.api.scenes.DynamicScene;
 import han.groepf.topdownshooter.World;
+import han.groepf.topdownshooter.entities.player.Player;
 
 public class GameScene extends DynamicScene implements EntitySpawnerContainer {
 
     private World world;
 
+    /**
+     * The basic game scene in which a controllable player entity has been added
+     * @param world
+     */
     public GameScene(World world) {
         this.world = world;
     }
@@ -19,7 +25,7 @@ public class GameScene extends DynamicScene implements EntitySpawnerContainer {
 
     @Override
     public void setupEntities() {
-
+        addEntity(new Player(new Coordinate2D(getWidth() * 0.1,getHeight() * 0.1)));
     }
 
     @Override
